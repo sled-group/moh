6452dd769fbe39c512254eb3c1d9c89479c7fef5
Xuweiyi Chen <62165079+XuweiyiChen@users.noreply.github.com>
2024-06-29 05:11:18 +0800

    Update minicpm-llama3-rope.sh

--- example-sh-files/minicpm-llama3-rope.sh
+++ example-sh-files/minicpm-llama3-rope.sh
@@ -5,9 +5,13 @@
 #SBATCH --ntasks=1
 #SBATCH --time=24:00:00
 #SBATCH --cpus-per-task=16
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=<your-email>
+#SBATCH --output=<your-output-repo-place>
+#SBATCH --error=<your--error-repo-place>
 
 module load cuda
-cd /scratch/rqa8sm/ROPE/moh
+cd /<your-path>/ROPE/moh
 
 # Initialize conda
 __conda_setup="$('/apps/software/standard/core/anaconda/2023.07-py3.11/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
@@ -26,4 +30,4 @@ unset __conda_setup
 conda activate /scratch/rqa8sm/ROPE/rope_env
 
 # Run the Python script
-python main.py --model_name minicpm --model_size "7b" --model_path /scratch/rqa8sm/ROPE/MiniCPM-Llama3-V-2_5 --device_map cuda --data_base_path /scratch/rqa8sm/ROPE/ROPE --output_base_path /scratch/rqa8sm/ROPE/output
+python main.py --model_name minicpm --model_size "7b" --model_path /<your-path>/ROPE/MiniCPM-Llama3-V-2_5 --device_map cuda --data_base_path /<your-path>/ROPE/ROPE --output_base_path /<your-path>/ROPE/output
