1cecf7b840d71866b4f520f2770d1c76e2b522f1
Xuweiyi Chen <62165079+XuweiyiChen@users.noreply.github.com>
2024-06-29 05:12:02 +0800

    Update yi-vl-34b-rope.sh

--- example-sh-files/yi-vl-34b-rope.sh
+++ example-sh-files/yi-vl-34b-rope.sh
@@ -5,9 +5,13 @@
 #SBATCH --ntasks=1
 #SBATCH --time=36:00:00
 #SBATCH --cpus-per-task=4
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=<your-email>
+#SBATCH --output=<your-output-repo-place>
+#SBATCH --error=<your--error-repo-place>
 
 module load cuda
-cd /scratch/rqa8sm/ROPE/moh
+cd /<your-path>/ROPE/moh
 
 # Initialize conda
 __conda_setup="$('/apps/software/standard/core/anaconda/2023.07-py3.11/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
@@ -23,7 +27,7 @@ fi
 unset __conda_setup
 
 # Activate the conda environment
-conda activate /scratch/rqa8sm/ROPE/rope_env
+conda activate /<your-path>/ROPE/rope_env
 
 # Run the Python script
-python main.py --model_name yivl --model_size "34b" --model_path /scratch/rqa8sm/ROPE/Yi-VL-34B-hf --device_map balanced --data_base_path /scratch/rqa8sm/ROPE/ROPE --output_base_path /scratch/rqa8sm/ROPE/output
+python main.py --model_name yivl --model_size "34b" --model_path /<your-path>/ROPE/Yi-VL-34B-hf --device_map balanced --data_base_path /<your-path>/ROPE/ROPE --output_base_path /<your-path>/ROPE/output
